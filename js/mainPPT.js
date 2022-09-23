5463ffdb2f3e8c4c8374d8e9cfa056d6ca281b92
angelDariaux <angeldariaux@gmail.com>
2022-09-22 22:32:34 -0600

    funciones de las jugadas

--- js/mainPPT.js
+++ js/mainPPT.js
@@ -1,39 +1,35 @@
 
-      // 1 es piedra, 2 es papel, 3 es tijera.
+    // Functions and global variables.
 
 const random = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1 ) + min)  // this creates a randon number between 1 up to 3.
+}
+
+const eleccion = (jugada) => { 
+  let resultado = "";
+  if ( jugada == 1 ) { 
+    resultado = "Piedra"
+  } else if ( jugada == 2 ) { 
+    resultado = "Papel"
+  } else if ( jugada == 3 ) { 
+    resultado = "Tijera"
+  } else {
+    resultado = "MAL ELEGIDO"
+  }
+  return resultado;
 }
 
 let jugador = 0;
 let pc = random(1, 3);  
 
 jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera")
-// alert(`Elegiste: ${jugador}`)
 
-    // Jugador options
-
-if ( jugador == 1 ) { 
-  alert("Elegiste  Piedra")
-} else if ( jugador == 2 ) { 
-  alert("Elegiste Papel")
-} else if ( jugador == 3 ) { 
-  alert("Elegiste Tijera")
-} else {
-  alert("Elegiste SUFRIMIENTO INTERMINABLE")
-}
-
-    // Pc options
-
-if ( pc == 1 ) { 
-  alert("PC  Piedra")
-} else if ( pc == 2 ) { 
-  alert("PC Papel")
-} else if ( pc == 3 ) { 
-  alert("PC Tijera")
-}
 
     // Fight !!! 
+
+alert(`Pc elige: ${eleccion(pc)}`);
+alert(`Tu eliges: ${eleccion(jugador)}`);
+
 
 if ( pc == jugador ) { 
   alert("EMPATE !!!")
