3b0c086d0de97ddfd9e7cdc51834e2cb2f82b2f8
angelDariaux <angeldariaux@gmail.com>
2022-09-22 22:11:17 -0600

    agregando numeros aleatorios

--- js/mainPPT.js
+++ js/mainPPT.js
@@ -1,8 +1,14 @@
 
-// 1 es piedra, 2 es papel, 3 es tijera.
+      // 1 es piedra, 2 es papel, 3 es tijera.
+
+const random = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1 ) + min)  // this creates a randon number between 1 up to 3.
+}
 
 let jugador = 0;
-let pc = 2
+let min = 1
+let max = 3
+let pc = random(min, max);  
 
 jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera")
 // alert(`Elegiste: ${jugador}`)
@@ -34,11 +40,11 @@ if ( pc == 1 ) {
 if ( pc == jugador ) { 
   alert("EMPATE !!!")
 } else if ( jugador == 1 && pc == 3 ) {
-  alert("GANASTE")
+  alert("GANASTE !!!")
 } else if ( jugador == 2 && pc == 1 ) { 
-  alert("GANASTE")
+  alert("GANASTE !!!")
 } else if ( jugador == 3 && pc == 2 ) {
-  alert("GANASTE")
+  alert("GANASTE !!!")
 } else { 
   alert("PERDISTE :( ")
 }
---
+++ js/minMax.js
@@ -0,0 +1,8 @@
+
+min = 15;
+max = 19;
+
+
+random = Math.floor(Math.random() * (max - min + 1 ) + min);
+console.log(random)
+
