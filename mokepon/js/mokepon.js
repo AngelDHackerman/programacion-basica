857e4009c5e695843685a3132ee92d37acaaa039
angelDariaux <angeldariaux@gmail.com>
2022-09-24 10:58:32 -0600

    seleccionando a los monkepones

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,8 +1,32 @@
 
-const selectionarMascotaJugador = () => { 
-  alert('Seleccionaste tu mascota !')
+const iniciarJuego = () => { 
+
+
+  const selectionarMascotaJugador = () => { 
+
+    const seleccionado = (mascota) => { 
+      return document.getElementById(mascota).checked
+    }
+
+    if ( seleccionado('hipodoge') ) { 
+      alert('Selectionaste a Hipodoge !')
+    } else if ( seleccionado('capipepo') ) {
+      alert('Seleccionaste a Capipepo')
+    } else if ( seleccionado('ratigueya') ) { 
+      alert('Seleccionaste a Ratiguyeya')
+    } else {
+      alert('No has selecionado nada !!!')
+    }
+  }
+
+
+  let botonMascotaJugador = document.getElementById('boton-mascota');
+  botonMascotaJugador.addEventListener('click', selectionarMascotaJugador);
+
+
+
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota');
-botonMascotaJugador.addEventListener('click', selectionarMascotaJugador);
 
+
+window.addEventListener('load', iniciarJuego)  // ? Asi podemos escuchar cuando el documento de Html este por completo cargado y ejecutar el JS 
