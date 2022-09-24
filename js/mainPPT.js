810f39c43e2c4e3680fe35003b8c01db8ae7a196
angelDariaux <angeldariaux@gmail.com>
2022-09-23 20:28:36 -0600

    agregado un loop infinito y mas funciones

--- js/mainPPT.js
+++ js/mainPPT.js
@@ -19,27 +19,48 @@ const eleccion = (jugada) => {
   return resultado;
 }
 
-let jugador = 0;
-let pc = random(1, 3);  
-
-jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera")
-
-
-    // Fight !!! 
-
-alert(`Pc elige: ${eleccion(pc)}`);
-alert(`Tu eliges: ${eleccion(jugador)}`);
-
-
-if ( pc == jugador ) { 
-  alert("EMPATE !!!")
-} else if ( jugador == 1 && pc == 3 ) {
-  alert("GANASTE !!!")
-} else if ( jugador == 2 && pc == 1 ) { 
-  alert("GANASTE !!!")
-} else if ( jugador == 3 && pc == 2 ) {
-  alert("GANASTE !!!")
-} else { 
-  alert("PERDISTE :( ")
+const sumar = (juego) => {
+  if (juego == "triunfos") { 
+    triunfos += 1;
+  } else { 
+    perdidas += 1;
+  }
 }
 
+let jugador = 0;
+let pc = 0;
+let triunfos = 0;
+let perdidas = 0;
+
+
+
+while ( triunfos < 3 && perdidas < 3 ) {
+  pc = random(1, 3);  
+  
+  jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera")
+  
+  
+      // Fight !!! 
+  
+  alert(`Pc elige: ${eleccion(pc)}`);
+  alert(`Tu eliges: ${eleccion(jugador)}`);
+  
+  
+  if ( pc == jugador ) { 
+    alert("EMPATE !!!")
+  } else if ( jugador == 1 && pc == 3 ) {
+    sumar("triunfos")
+    alert("GANASTE !!!")
+  } else if ( jugador == 2 && pc == 1 ) { 
+    sumar("triunfos")
+    alert("GANASTE !!!")
+  } else if ( jugador == 3 && pc == 2 ) {
+    sumar("triunfos")
+    alert("GANASTE !!!")
+  } else { 
+    sumar("perdidas")
+    alert("PERDISTE :( ")
+  }
+}  
+
+alert(`Ganaste: ${triunfos} veces. Perdiste: ${perdidas} veces.`)
