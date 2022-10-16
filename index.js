b2974b7680cb84653b5532a82b2d4637c01a3083
angelDariaux <angeldariaux@gmail.com>
2022-10-16 14:47:41 -0600

    creado el primero hola mundo en express js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('holaaaaaaaaa')
+
+const express = require('express')  // llamamos a la libreria de express
+
+const app = express() // inicamos express
+
+app.get('/', (req, res) => { // Esta funcion escucha nuestras peticiones echas en la URL "/" que es el localhost, en el puerto 8080.
+  res.send('Hooolaa')
+})
+
+app.listen(8080, () => { // Aqui le decimos que puerto debe escuchar nuestra aplicacion
+  console.log('Servidor funcionando')
+})
